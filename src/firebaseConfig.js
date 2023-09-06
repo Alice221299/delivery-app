5cfaa431074d0c0331988c5981ccb11b7d9f9754
Ana María <developeranamaria@gmail.com>
2023-09-06 18:25:13 -0500

    firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore'
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
+
 // const firebaseConfig = {
 //   apiKey: "AIzaSyA8W3FAB_p2GnMJKdoK7S4nRrxojmUWAFM",
 //   authDomain: "delivery-app-9a81b.firebaseapp.com",
@@ -16,10 +16,12 @@
 // };
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// export const app = initializeApp(firebaseConfig);
 // export const auth = getAuth(app);
 // export const firestore = getFirestore(app);
 
+
+// New Firebase
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -29,22 +31,13 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAwhCNa5uQjsuncC2frnb0YfSRMBY6Id2s",
-//   authDomain: "crud-react-firebase-front-5.firebaseapp.com",
-//   projectId: "crud-react-firebase-front-5",
-//   storageBucket: "crud-react-firebase-front-5.appspot.com",
-//   messagingSenderId: "1010880102199",
-//   appId: "1:1010880102199:web:07b8a5d7b60a893eace4b2",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA8W3FAB_p2GnMJKdoK7S4nRrxojmUWAFM",
-  authDomain: "delivery-app-9a81b.firebaseapp.com",
-  projectId: "delivery-app-9a81b",
-  storageBucket: "delivery-app-9a81b.appspot.com",
-  messagingSenderId: "50705869773",
-  appId: "1:50705869773:web:505855e2bc05db5520b284"
+  apiKey: "AIzaSyAqV5woy-9nlbHCNyLQ5rrWuAyx_qa9J88",
+  authDomain: "deliveryapp-a8232.firebaseapp.com",
+  projectId: "deliveryapp-a8232",
+  storageBucket: "deliveryapp-a8232.appspot.com",
+  messagingSenderId: "534722210056",
+  appId: "1:534722210056:web:79c711db1dacb5b191a4bd"
 };
 
 // Initialize Firebase
