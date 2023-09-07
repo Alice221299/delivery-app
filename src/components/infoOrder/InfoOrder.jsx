ef80790379e925bfb07be75eb3d7f18886ca1a4f
Anyerson <lorddeep3@gmail.com>
2023-09-07 14:22:59 -0500

    Final Commit Order

--- src/components/infoOrder/InfoOrder.jsx
+++ src/components/infoOrder/InfoOrder.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './infoOrder.scss'
 
-const InfoOrder = () => {
+const InfoOrder = ({cantidad, name, valor}) => {
   return (
     
     <section className='infoOrderOne'>
         <div className='infoOrderOne__product'>
-            <span className='quantityOrder'><b>1x</b></span>
-            <span className='nameOrderInfo'>Meat Pizza(medium)</span>
+            <span className='quantityOrder'><b>{cantidad}</b></span>
+            <span className='nameOrderInfo'>{name}</span>
         </div>
         
-        <span className='infoOrderOne__price'>$35.50</span>
+        <span className='infoOrderOne__price'>$ {valor}</span>
     </section>
 
   )
