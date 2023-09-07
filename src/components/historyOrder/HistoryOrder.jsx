b015543e241a2b927dc45f8ead18783b04bdccef
Anyerson <lorddeep3@gmail.com>
2023-09-07 13:58:18 -0500

    Orders and Users

--- src/components/historyOrder/HistoryOrder.jsx
+++ src/components/historyOrder/HistoryOrder.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import './historyOrder.scss'
 import { useNavigate } from 'react-router-dom'
+import { fillRestaurantsFromCollection } from "../../redux/actions/restaurantsActions";
+
+const HistoryOrder = ({idRestaurant, total, statu, idOrder}) => {
+
+    const { restaurants } = useSelector((store) => store.restaurants);
+
+    const [res, setRes] = useState([]);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fillRestaurantsFromCollection());
+      }, []);
 
 
-const HistoryOrder = () => {
+    useEffect (() => 
+    {
+            console.log('El restaurante es: ', restaurants)
+
+            if (restaurants.length > 0) 
+            {
+                setRes(restaurants.filter(order => order.id == idRestaurant) );
+            } 
+    }, [restaurants])
+
+    useEffect(()=> 
+    {
+            if (res.length >0 )
+            {
+                console.log('El restaurante es: ', res)
+            }
+    }, [res])
 
     const navigate = useNavigate();
     const handleNavigate = (ruta) => 
@@ -12,23 +42,24 @@ const HistoryOrder = () => {
     }
     return (
         <span className='oneOrder'>
+            
             <section className='detailOrder'>
 
-                <figure className='detailOrder__figure'>
-                    <img className='orderImg' src="https://i.ibb.co/FmqdgyY/image.png" alt="Restaurant" />
-                </figure>
+                { res.length >0 && <figure className='detailOrder__figure'>
+                    <img className='orderImg' src= {res[0].image} alt="Restaurant" />
+                </figure>}
 
                 <span className='restaurant'>
-                    <span className='restaurant__name'><b>Pardes restaurant</b></span>
-                    <span className='restaurant__price'><b>$ 132.00</b></span>
+                    {res.length > 0 && <span className='restaurant__name'><b>{res[0].name}</b></span>}
+                    <span className='restaurant__price'><b>$ {total}</b></span>
                 </span>
 
             </section>
 
             <section className='statuOrders'>
 
-                <span className='statuOrder'><b>Delivery</b></span>
-                <svg onClick={() => handleNavigate('/OrderData')} className='arrowOrder' xmlns="http://www.w3.org/2000/svg" width="5" height="8" viewBox="0 0 5 8" fill="none">
+                <span className='statuOrder'><b>{statu}</b></span>
+                <svg onClick={() => handleNavigate(`/OrderData/${idOrder}`)} className='arrowOrder' xmlns="http://www.w3.org/2000/svg" width="5" height="8" viewBox="0 0 5 8" fill="none">
                     <path d="M4.35355 4.35355C4.54882 4.15829 4.54882 3.84171 4.35355 3.64645L1.17157 0.464466C0.976311 0.269204 0.659728 0.269204 0.464466 0.464466C0.269204 0.659728 0.269204 0.976311 0.464466 1.17157L3.29289 4L0.464466 6.82843C0.269204 7.02369 0.269204 7.34027 0.464466 7.53553C0.659728 7.7308 0.976311 7.7308 1.17157 7.53553L4.35355 4.35355ZM3 4.5H4V3.5H3V4.5Z" fill="#414141" />
                 </svg>
 
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -5,7 +5,24 @@ import EngineSearch from '../../components/engineSearch/EngineSearch'
 import FooterSearch from '../../components/footerSearch/FooterSearch'
 import RecentSearches from '../../components/recentSearches/RecentSearches'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fillRestaurantsFromCollection } from '../../redux/actions/restaurantsActions'
+import { fillProductsFromCollection } from '../../redux/actions/productsActions'
 const Search = () => {
+
+  const { products } = useSelector(store => store.products);
+  const dispatch = useDispatch();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => 
+          { 
+            dispatch(fillRestaurantsFromCollection()) .then(() => 
+            dispatch(fillProductsFromCollection())) .then(() => 
+            console.log('estos son los Productos', products)); 
+          }, []);
+
+
+
 
 
   const navigate = useNavigate();
@@ -20,26 +37,28 @@ const Search = () => {
     console.log('El nuevo valor del estado es: ', searchValue);
   }, [searchValue])
 
+  useEffect(() => 
+        { 
+          console.log('El nuevo valor del estado es: ', searchValue);
+        
+          setFilteredProducts(products.filter(product => product.name.toLowerCase().includes(searchValue.toLowerCase())) );
+
+        }, [searchValue, products])
+
 
   return (
     <div className='searchMain'>
       <EngineSearch getInput={(value) => setSearchValue(value)} />
       {searchValue !== '' && <section className='result'>
-        <ElementSearch />
-        <ElementSearch />
-        <ElementSearch />
-        <ElementSearch />
-        <ElementSearch />
+      {filteredProducts.length  > 0 &&  filteredProducts.map(product => ( <ElementSearch key={product.id} 
+  name={product.name} price={product.price} id={product.id} image={product.image} /> ))}
       </section>}
       {searchValue === '' && <section className='recentSearches'>
         <span className='titleRecent'><b>Recent searches</b></span>
         <RecentSearches />
-        <RecentSearches />
-        <RecentSearches />
-        <RecentSearches />
-        <RecentSearches />
 
       </section>}
+
       <FooterSearch statu={2}/>
 
     </div>
